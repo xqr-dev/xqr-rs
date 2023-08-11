1f4fbc2d5de1017f2cc77b3627ca276c5d7b2172
Elijah Wilson <dev.tizz98@gmail.com>
2023-08-10 22:47:53 -0400

    docs

--- src/lib.rs
+++ src/lib.rs
@@ -2,29 +2,44 @@ use jwt_simple::prelude::*;
 use jwt_simple::Error;
 use serde_derive::{Deserialize, Serialize};
 
+/// Represents the extended quick response (XQR) code, encapsulating the JWT token.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct XQR {
     pub token: String,
 }
 
+/// Converts the XQR structure to a String representation.
 impl ToString for XQR {
     fn to_string(&self) -> String {
         self.token.clone()
     }
 }
 
+/// Converts a String to an XQR structure.
 impl From<String> for XQR {
     fn from(s: String) -> Self {
         XQR { token: s }
     }
 }
 
+/// Defines the custom claims to be used in the JWT token, including `kid` and `value`.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct XQRClaims {
     kid: String,
     value: String,
 }
 
+/// Encodes a value into a JWT token using a private key in PEM format.
+///
+/// # Arguments
+///
+/// * `private_key_pem` - The private key in PEM format.
+/// * `value` - The value to encode into the JWT token.
+/// * `kid` - The key ID, typically a URL that identifies the key.
+///
+/// # Returns
+///
+/// A Result containing the XQR structure or an error if the operation fails.
 pub fn encode(private_key_pem: &str, value: &str, kid: &str) -> Result<XQR, Error> {
     let key_pair = ES256KeyPair::from_pem(private_key_pem)?;
     let claims = Claims::with_custom_claims(
@@ -39,6 +54,16 @@ pub fn encode(private_key_pem: &str, value: &str, kid: &str) -> Result<XQR, Erro
     Ok(XQR { token })
 }
 
+/// Decodes a JWT token contained in an XQR structure using a public key in PEM format.
+///
+/// # Arguments
+///
+/// * `public_key_pem` - The public key in PEM format.
+/// * `xqr` - The XQR structure containing the JWT token to decode.
+///
+/// # Returns
+///
+/// A Result containing the decoded value as a String or an error if the operation fails.
 pub fn decode(public_key_pem: &str, xqr: XQR) -> Result<String, Error> {
     let public_key = ES256PublicKey::from_pem(public_key_pem)?;
     let token = xqr.token;
@@ -47,6 +72,11 @@ pub fn decode(public_key_pem: &str, xqr: XQR) -> Result<String, Error> {
     Ok(claims.value)
 }
 
+/// Generates a new ECDSA (ES256) key pair for use with JWT tokens.
+///
+/// # Returns
+///
+/// The generated ES256 key pair.
 pub fn generate_key_pair() -> ES256KeyPair {
     ES256KeyPair::generate()
 }
